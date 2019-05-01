279ed6cd7e9109ba5837546e18b79bc338284391
unknown <Digital@DH-A2-33.dhbrasil.digitalhouse.com>
2019-04-30 22:10:11 -0300

    linkado com o banco de dados

---
+++ req/database.php
@@ -0,0 +1,10 @@
+<?php
+    $dsn = "mysql:host=localhost;dbname=sys;charset=utf8mb4;port:3306";
+    $db_user = "root";
+    $db_pass = "root";
+
+    // $conexao = new PDO($dsn, $db_user);
+    // var_dump($conexao);
+
+    // $conexao -> query('SELECT * FROM usuarios');
+?>
